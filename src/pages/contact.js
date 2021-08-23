6f367d7b2235e88ba6c09e6e4ca782b35152f936
Martina Tavoda <martinatavoda@Martinas-Mac-mini.local>
2021-08-23 10:23:52 +0200

    addin pages

---
+++ src/pages/contact.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Contact = () => {
+  return <h1>Hello from Contacts</h1>;
+};
+
+export default Contact;
---
+++ src/pages/projects.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Projects = () => {
+  return <h1>Hello from projects</h1>;
+};
+
+export default Projects;
