00f12c379903443e40b181655232342758c3547b
Martina Tavoda <martinat@tutanota.com>
2021-08-27 10:28:13 +0200

    fixing footer styling mobile view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,13 @@ import * as React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <h4>
-        copyright &copy; {new Date().getFullYear()} <span>O</span>
-        rigins all rights reserved
-      </h4>
-      <h4>Made with ☕ by MT Web Development & Design</h4>
+      <div className="footer-wrapper">
+        <h4>
+          copyright &copy; {new Date().getFullYear()} <span>O</span>
+          rigins all rights reserved
+        </h4>
+        <h4>Made with ☕ by MT Web Development & Design</h4>
+      </div>
     </footer>
   );
 };
